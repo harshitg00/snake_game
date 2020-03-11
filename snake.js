7091401501d90b758cbe9d7ec240d7c2a8b6afe3
harris2310 <harris2310@hotmail.com>
2020-03-11 15:43:53 +0200

    Commit

--- snake.js
+++ snake.js
@@ -43,22 +43,21 @@ let score = 0;
 
 //control the snake
 
-let last;
 let d;
 document.addEventListener("keydown",direction);
 
 function direction(event){
     let key = event.keyCode;
-    if(key == 37 && last!="right" && last!="left"){
+    if(key == 37 && d!="right" && d!="left"){
         d="left";
         left.play();
-    } else if(key == 38 && last!="down" && last!="up"){
+    } else if(key == 38 && d!="down" && d!="up"){
         d="up";
         up.play();
-    }else if(key == 39 && last!="left" && last!= "right"){
+    }else if(key == 39 && d!="left" && d!= "right"){
         d= "right";
         right.play();
-    }else if(key == 40 && last!="up" && last!="down"){
+    }else if(key == 40 && d!="up" && d!="down"){
         d="down";
         down.play();
     }
@@ -78,32 +77,26 @@ function draw(){
         ctx.fillStyle = (i==0)? "green":"white";
         ctx.fillRect(snake[i].x,snake[i].y,box,box);
     }
-    while(collision(food,snake) == true){
-        food = {
-            x: Math.floor(Math.random()*17+1)*box,
-            y: Math.floor(Math.random()*15+3)*box
-        }
-    }
     ctx.drawImage(foodImg,food.x,food.y);
 
     //old head position
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
+    
 
     //which direction
     if(d=="left") snakeX -=box;
     if(d=="up") snakeY -=box;
     if(d=="right") snakeX +=box;
     if(d=="down") snakeY +=box;
-    last = d;
 
     //if th snake eats food
     if(snakeX == food.x && snakeY == food.y){
         score++;
         eat.play();
         food = {
-            x: Math.floor(Math.random()*17+1)*box,
-            y: Math.floor(Math.random()*15+3)*box
+        x: Math.floor(Math.random()*17+1)*box,
+        y: Math.floor(Math.random()*15+3)*box
         }
     } else {
         //remove the tail
