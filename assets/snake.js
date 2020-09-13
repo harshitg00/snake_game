d80e9d22047aba4ebc218d69cd17f84b677d6de2
Harshit Gupta <lcs2019008@iiitl.ac.in>
2020-09-13 13:02:10 +0530

    Smoother play

--- assets/snake.js
+++ assets/snake.js
@@ -58,6 +58,13 @@ let last;
 let d;
 document.addEventListener("keydown",direction);
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 function direction(event){
     let key = event.keyCode;
     if(key == 37 && last!="right" && last!="left"){
